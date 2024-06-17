7caeb2c5712fe79b7712dcdffd3f3ac145095c16
Jayparmar <jayparmar7271@gmail.com>
2024-06-17 23:10:18 +0530

    fixed header issue

--- assest/javascript/app.js
+++ assest/javascript/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var responsiveMenu = document.querySelector(".responsive-menu");
     var mobileMenu = document.querySelector(".mobile-menu");
 
+    // Add click event listener to toggle 'active' class on responsive-menu and mobile-menu
     responsiveMenu.addEventListener("click", function() {
         this.classList.toggle("active");
         mobileMenu.classList.toggle("active");
